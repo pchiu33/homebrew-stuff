8792c0d510a83b0cfa7394a619d027e4bc2d2b4b
Michael Blakeley <mike@blakeley.com>
2013-08-16 19:17:49 -0700

    ansible has no master branch, so specify devel for head

--- ansible.rb
+++ ansible.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Ansible < Formula
   homepage 'http://ansible.github.com/'
-  head 'https://github.com/ansible/ansible.git', :using => :git
+  head 'https://github.com/ansible/ansible.git', :branch => :devel
   url 'https://github.com/ansible/ansible/archive/v1.2.2.tar.gz'
   sha1 'cd64c200edec22b9eb0581a79491a7aa551cc864'
 
